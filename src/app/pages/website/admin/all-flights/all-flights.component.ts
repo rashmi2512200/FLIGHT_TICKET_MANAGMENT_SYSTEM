9da3c0be9a4ef970dad11e2414d206eee8afe115
rashmi2512200 <90035725+rashmi2512200@users.noreply.github.com>
2024-03-05 16:38:36 +0530

    first commit

---
+++ src/app/pages/website/admin/all-flights/all-flights.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-flights',
+  standalone: true,
+  imports: [],
+  templateUrl: './all-flights.component.html',
+  styleUrl: './all-flights.component.css'
+})
+export class AllFlightsComponent {
+
+}
---
+++ src/app/pages/website/book-flight/book-flight.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-flight',
+  standalone: true,
+  imports: [],
+  templateUrl: './book-flight.component.html',
+  styleUrl: './book-flight.component.css'
+})
+export class BookFlightComponent {
+
+}
